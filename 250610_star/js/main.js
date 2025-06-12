83fcc24f4e84b35cfede4c6ad70d6fddb1d3730c
aji-o <ktu1019@gmail.com>
2025-06-12 21:05:14 +0900

    250611

--- 250610_star/js/main.js
+++ 250610_star/js/main.js
@@ -1,39 +1,47 @@
 $(document).ready(function(){
 
-    //header
+    //header 
 
-    $('header .gnb ul.depth1 > li').on('mouseenter focusin', function(){
-        $('header .gnb ul.depth1 > li').removeClass('over')
+    $('header .gnb > ul.depth1 > li').on('mouseenter focusin', function(){
+        $('header .gnb > ul.depth1 > li').removeClass('over')
         $(this).addClass('over')
     })
 
-    $('header .gnb ul.depth1 > li').on('mouseleave', function(){
-        $('header .gnb ul.depth1 > li').removeClass('over')
+        $('header .gnb > ul.depth1 > li').on('mouseleave', function(){
+        $('header .gnb > ul.depth1 > li').removeClass('over')
     })
 
-
-    //visual
+    //slide
 
     let idx = 1
-    
+
     setInterval(() => {
-        if(idx < 3){
+        if (idx < 3){
             idx++
         }else{
             idx = 1
         }
-        //console.log(idx)
-        $('.visual ul li').removeClass('active')
-        $('.visual ul li').eq(idx - 1).addClass('active')
+        $('.slide ul li').removeClass('active')
+        $('.slide ul li').eq(idx -1).addClass('active')
     }, 3000);
 
 
-
-    //news 
+    //news
 
     $('.contents .news > ul > li').on('click', function(){
         $('.contents .news > ul > li').removeClass('active')
         $(this).addClass('active')
     })
 
-})
+
+    //layer_popup
+
+    $('.contents .news > ul > li.notice > .detail > ul > li > a.popup_btn').on('click', function(){
+        $('.layer_popup').show()
+    })
+    $('.layer_popup .close').on('click', function(){
+    $('.layer_popup').hide()
+    })
+
+
+}) //$(document).ready
